66a4cd0c5a3b5fd2f9047cb1ef7f0c2bd4e91fbe
chaeheonjeong <cogjs3210@naver.com>
2024-06-14 08:35:20 +0900

    feat: 매물 조회, 즐겨찾기 CRUD

---
+++ src/main/java/com/billybang/propertyservice/dto/request/StarredPropertyReqeustDto.java
@@ -0,0 +1,10 @@
+package com.billybang.propertyservice.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StarredPropertyReqeustDto {
+    private Long propertyId;
+}
