8df31e01056ebf34214b2fb983f1fd312dadacd8
chaeheonjeong <cogjs3210@naver.com>
2024-06-24 22:05:28 +0900

    feat: 성능 개선

--- src/main/java/com/billybang/propertyservice/repository/PropertyRepository.java
+++ src/main/java/com/billybang/propertyservice/repository/PropertyRepository.java
@@ -1,13 +1,23 @@
 package com.billybang.propertyservice.repository;
 
 import com.billybang.propertyservice.model.entity.Property;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PropertyRepository extends JpaRepository<Property, Long>{
+
+    @Query("select p from Property p where p.latitude <= :topLat " +
+            "and p.latitude >= :bottomLat " +
+            "and p.longitude <= :rightLon " +
+            "and p.longitude >= :leftLon")
+    List<Property> findPropertiesByRange(@Param("topLat") double topLat,
+                                         @Param("bottomLat") double bottomLat,
+                                         @Param("rightLon") double rightLon,
+                                         @Param("leftLon") double leftLon);
+
+    List<Property> findByAreaId(Long areaIds);
 }
