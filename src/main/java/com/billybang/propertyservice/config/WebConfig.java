dd634e5c90a88117ec92a9dd31128b0e4cf301c1
jinhyeon <vmflwu123@gmail.com>
2024-06-25 17:03:35 +0900

    fix: cors 설정 수정

--- src/main/java/com/billybang/propertyservice/config/WebConfig.java
+++ src/main/java/com/billybang/propertyservice/config/WebConfig.java
@@ -9,10 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://wwww.billybang.me")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Authorization", "Content-Type", "Cookie")
-                .exposedHeaders("Authorization", "Cookie")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Authorization")
                 .allowCredentials(true);
     }
 }
